ab8b2ecf50cd40f59e78d1e78b456ea592195ad5
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-02-09 01:50:02 +0100

    Added files
    hw_tricks

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config                      = Config or {}
+
+Config.Eject                = true      -- This value determines whether the player will be ejected if they land while doing a trick or not (The default value is true)
+Config.InstantAnimStop      = true      -- This value determines whether the anim should instantly stopped, otherwise it will just go back to the sitting position gradually (The default value is true)
+
+Config.SpecificVehicle      = false      -- If set to true, you will be able to do tricks only on some specific bikes from the 'Config.Vehicles' list
+Config.Vehicles             = {         -- Don't use "Model Name" since there is no native to get model name from vehicle in GTA, only use "Text Label Name(s)". You can found them here: https://gta.fandom.com/wiki/Category:Motorcycles_Vehicle_Class
+    "ENDURO",
+    "ESSKEY",
+    "MANCHEZ",
+    "SANCHEZ01",
+    "SANCHEZ02"
+}
+
+Config.SimpleKey            = 73   -- You can find value here: https://docs.fivem.net/docs/game-references/controls/
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'HenkW'
+description 'Tricks resource for FiveM servers'
+version '1.1.0'
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
+
+server_scripts {
+	'config.lua',
+	'server/main.lua'
+}
+
+fivem_checker 'yes'
